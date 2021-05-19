d46c25e6f3761fed4ed3a49b55b4a51beb218525
Matt Johnsen <johnsenwriting.gmail.com>
2021-05-19 10:16:48 -0500

    hijkjn

--- design.js
+++ design.js
@@ -2,7 +2,7 @@ const bars = document.querySelector("#menu");
 const drop = document.querySelector(".dropdown-content");
 const dropItems = document.querySelectorAll(".nav-item");
 
-bars.addEventListener("click", function(){
+bars.addEventListener("touchend", function(){
     drop.style.visibility = "visible";
     drop.style.display = "block";
     for (let i = 0; i < dropItems.length; i++){
